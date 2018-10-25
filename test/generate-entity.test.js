6762b6b06eaaee4c1fb3d91bdbf62bb8ec82013c
Olga Kotova <oliktva@gmail.com>
2018-10-25 23:37:36 +0300

    fix tests

--- test/generate-entity.test.js
+++ test/generate-entity.test.js
@@ -7,13 +7,13 @@ const PlaceValue = {
   MIN_PRICE: 1000,
   MAX_PRICE: 1000000,
   MIN_ROOMS: 1,
-  MAX_ROOMS: 5,
+  MAX_ROOMS: 1000,
   MIN_FEATURES: 1
 };
 
 const PlaceData = {
   OFFER_TITLES: [`Большая уютная квартира`, `Маленькая неуютная квартира`, `Огромный прекрасный дворец`, `Маленький ужасный дворец`, `Красивый гостевой домик`, `Некрасивый негостеприимный домик`, `Уютное бунгало далеко от моря`, `Неуютное бунгало по колено в воде`],
-  OFFER_TYPES: [`flat`, `house`, `bungalo`],
+  OFFER_TYPES: [`flat`, `house`, `bungalo`, `palace`],
   CHECK_TIMES: [`12:00`, `13:00`, `14:00`],
   OFFER_FEATURES: [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`],
   PHOTOS: [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`]
